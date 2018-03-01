0b38f070b50ed0d3838ed70b3ed401907defc40e
Pablo Tato Ramos <pabtatoramos@gmail.com>
2018-03-01 20:08:34 +0100

    AAAAA

--- q_round.py
+++ q_round.py
@@ -1,14 +1,14 @@
-[R, C, F, N, B, T] = input().split()
+(R, C, F, N, B, T) = (int(x) for x in input().split()) 
 rides = []
 
 class Ride:
     def __init__(self, ride):
-        [a, b, x, y, s, f] = ride
+        (a, b, x, y, s, f) = (int(x) for x in ride)
         self.a, self.b, self.x, self.y, self.s, self.f = a, b, x, y, s, f
 
 for i in range(N):
     rides.append(Ride(input().split()))
 
 
-
-
+rides = sorted(rides, key=lambda r: r.s)
+print(rides)
